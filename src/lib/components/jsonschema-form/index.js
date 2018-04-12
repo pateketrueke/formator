58e0404cb79d8e16bb065de76d5ef9ee4b215e59
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-04-12 01:08:21 -0500

    Upgrade deps

--- src/lib/components/jsonschema-form/index.js
+++ src/lib/components/jsonschema-form/index.js
@@ -3,18 +3,18 @@ const loadScript = document.currentScript.import;
 let jsonschemaForm;
 
 loadScript([
-  '//cdnjs.cloudflare.com/ajax/libs/react/16.0.0/umd/react.development.js',
-  '//cdnjs.cloudflare.com/ajax/libs/react-dom/16.0.0/umd/react-dom.development.js',
+  '//unpkg.com/react@16.3.1/umd/react.production.min.js',
+  '//unpkg.com/react-dom@16.3.1/umd/react-dom.production.min.js',
   '//unpkg.com/react-jsonschema-form@0.50.1/dist/react-jsonschema-form.js',
   '//unpkg.com/classnames@2.2.5/index.js',
   '//unpkg.com/prop-types@15.5.10/prop-types.js',
-  '//unpkg.com/react-input-autosize@2.0.0/dist/react-input-autosize.js',
-  '//unpkg.com/react-select/dist/react-select.js',
-  '//unpkg.com/react-select/dist/react-select.css',
+  '//unpkg.com/react-input-autosize@2.0.0/dist/react-input-autosize.min.js',
+  '//unpkg.com/react-select@1.2.1/dist/react-select.min.js',
+  '//unpkg.com/react-select@1.2.1/dist/react-select.css',
   '//unpkg.com/react-json-view@1.16.1/dist/main.js',
-  '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.1/moment.min.js',
-  '//cdnjs.cloudflare.com/ajax/libs/react-datetime/2.10.3/react-datetime.js',
-  '//cdnjs.cloudflare.com/ajax/libs/react-datetime/2.10.3/css/react-datetime.css',
+  '//unpkg.com/moment@2.22.0/moment.js',
+  '//unpkg.com/react-datetime@2.14.0/dist/react-datetime.min.js',
+  '//unpkg.com/react-datetime@2.14.0/css/react-datetime.css',
 ]);
 
 loadScript('jsonschema-form/resource').then(resourceFactory => {
