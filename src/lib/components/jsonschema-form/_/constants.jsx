35b416a35022aeecb2a3e34165145eaf19e999a3
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-05-03 00:55:38 -0500

    Add mul helper

--- src/lib/components/jsonschema-form/_/constants.jsx
+++ src/lib/components/jsonschema-form/_/constants.jsx
@@ -93,6 +93,22 @@ export const TYPES = {
   sum(data, values) {
     return values.map(x => x.reduce((prev, cur) => prev + cur, 0)).join(', ');
   },
+  mul(data, values) {
+    const isMixed = Array.isArray(values[0]);
+    const length = isMixed ? values[0].length : 1;
+
+    if (isMixed) {
+      values = values.map(x => x.map(Number).reduce((prev, cur) => prev + cur, 0));
+    }
+
+    return values
+      .reduce((prev, cur) => {
+        if (cur !== 0 && cur !== Infinity) {
+          prev *= cur;
+        }
+        return prev || cur;
+      }, 0) / length;
+  },
   uniq(data, values) {
     return (this.value(data, values) || [])
       .reduce((prev, cur) => {
