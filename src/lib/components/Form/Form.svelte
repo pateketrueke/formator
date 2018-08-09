9440685be288dff20abfa82601dde67020477b9f
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-08-09 16:31:13 -0500

    Iterate simple fields; WIP

--- src/lib/components/Form/Form.svelte
+++ src/lib/components/Form/Form.svelte
@@ -1,24 +1,45 @@
-<h1>FORM</h1>
-
-<ul>
-  {#each Object.entries(schema.properties) as [prop, field]}
-    <li><Field name={prop} props={field} /></li>
-  {:else}
-    <li>NO PROPS</li>
-  {/each}
-</ul>
+<form {...props}>
+  <ul>
+    {#each fields as [name, props]}
+      <li><Field {name} {props} /></li>
+    {:else}
+      <li>NO PROPS</li>
+    {/each}
+  </ul>
+  <p>
+    <button type="submit">Save</button>
+  </p>
+  <pre>{JSON.stringify(currentAction)}</pre>
+  <pre>{JSON.stringify(nextAction)}</pre>
+</form>
 
 <script>
 import Field from '../Field';
+
+const ACTION_MAP = {
+  new: 'create',
+};
 
 export default {
   components: {
     Field,
   },
-  oncreate() {
-    console.log(this.options.data);
+  computed: {
+    currentAction({ model, action, actions }) {
+      return actions[model][action];
+    },
+    nextAction({ model, action, actions }) {
+      return actions[model][ACTION_MAP[action]];
+    },
+    fields({ schema }) {
+      return Object.entries(schema.properties);
+    },
+    props({ nextAction }) {
+      return {
+        method: nextAction.verb.toLowerCase(),
+        action: nextAction.path,
+      };
+    },
   },
 };
 </script>
-
-
