9ffab5c23bbe6d24c29ae57f29b3a07854264f62
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-08-14 00:36:05 -0500

    Modal impl

---
+++ src/lib/components/Modal/Modal.svelte
@@ -0,0 +1,64 @@
+{#if visible}
+  <div data-modal on:click="cancel(event)">
+    <div data-content>
+      <slot></slot>
+      <button type="button" on:click="save()">Save</button>
+      or
+      <a href="#" on:click="close(event)">Cancel</a>
+    </div>
+  </div>
+{/if}
+
+<script>
+const STACK = [];
+
+window.addEventListener('keyup', e => {
+  if (e.keyCode === 27) {
+    const last = STACK.pop();
+
+    if (last) {
+      last.close();
+    }
+  }
+});
+
+export default {
+  ondestroy() {
+    const offset = STACK.indexOf(this);
+
+    if (offset !== -1) {
+      STACK.splice(offset, 1);
+    }
+  },
+  onupdate() {
+    const { visible } = this.get();
+
+    if (visible) {
+      STACK.push(this);
+    } else {
+      STACK.splice(STACK.indexOf(this), 1);
+    }
+  },
+  methods: {
+    cancel(e) {
+      if (e && e.target && !e.target.dataset.modal) {
+        return;
+      }
+
+      this.close(e);
+    },
+    close(e) {
+      if (e) {
+        e.preventDefault();
+      }
+
+      this.fire('close');
+      this.set({ visible: false });
+    },
+    save() {
+      this.fire('save');
+      this.close();
+    },
+  },
+};
+</script>
