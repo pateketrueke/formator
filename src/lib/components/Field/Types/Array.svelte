e89e549ebac14af7a48c2cbbc2582a671358782e
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-08-14 00:35:48 -0500

    Handle through associations for Array types; WIP

--- src/lib/components/Field/Types/Array.svelte
+++ src/lib/components/Field/Types/Array.svelte
@@ -1,29 +1,40 @@
 {#if items.length}
-  <fieldset>
-    <ul>
-      {#each items as { key, props, offset, isFixed } (key)}
-        <li data-type={props.type || 'object'}>
-          <div data-item>
-            <div>
-              <Field {props} bind:result="result[offset]" name={`${name}[${offset}]`} />
-            </div>
-            {#if !isFixed}
+  {#if through}
+    <pre>{JSON.stringify(items, null, 2)}</pre>
+  {:else}
+    <fieldset>
+      <ul>
+        {#each items as { key, props, offset, isFixed } (key)}
+          <li data-type={props.type || 'object'}>
+            <div data-item>
               <div>
-                <button data-remove="&times;" type="button" on:click="remove(offset)">
-                  <span>Remove item</span>
-                </button>
+                <Field {props} bind:result="values[offset]" name={`${name}[${offset}]`} />
               </div>
-            {/if}
-          </div>
-        </li>
-      {/each}
-    </ul>
-  </fieldset>
+              {#if !isFixed}
+                <div>
+                  <button data-remove="&times;" type="button" on:click="remove(offset)">
+                    <span>Remove item</span>
+                  </button>
+                </div>
+              {/if}
+            </div>
+          </li>
+        {/each}
+      </ul>
+    </fieldset>
+  {/if}
 {:else}
   <div data-empty>NO ITEMS</div>
 {/if}
 
-{#if schema.additionalItems !== false}
+{#if through}
+  <div>
+    <button type="button" on:click="open()">Add {association.singular}</button>
+    <Modal bind:visible="isOpen" on:save="sync()">
+      <Field {...nextProps} bind:result="nextValue" name={`${name}[${nextOffset}]`} />
+    </Modal>
+  </div>
+{:elseif schema.additionalItems !== false}
   <div>
     <button data-append="&plus;" type="button" on:click="append()">
       <span>Add item</span>
@@ -46,35 +57,58 @@ function getProps(schema, offset) {
 export default {
   components: {
     Field: '../Field',
+    Modal: '../../Modal',
   },
   data() {
     return {
       keys: [],
-      result: [],
+      result: null,
     };
   },
   oncreate() {
-    const { result, schema } = this.get();
+    const { values, schema } = this.get();
 
     if (schema && Array.isArray(schema.items)) {
       const keys = [];
-      const values = [];
+      const result = [];
 
       schema.items.forEach((props, offset) => {
         keys.push(randId());
-        values.push(result[offset] || defaultValue(props));
+        result.push(values[offset] || defaultValue(props));
       });
 
-      this.set({ result: values, keys });
+      this.set({ result, keys });
     }
   },
   methods: {
     append() {
-      const { result, schema, keys } = this.get();
+      const { nextOffset, schema } = this.get();
+
+      this.add(defaultValue(getProps(schema, nextOffset)));
+    },
+    remove(offset) {
+      const { result, keys } = this.get();
+
+      result.splice(offset, 1);
+      keys.splice(offset, 1);
+
+      this.set({ result, keys });
+    },
+    open() {
+      const { schema, nextOffset } = this.get();
+
+      this.set({
+        isOpen: true,
+        nextValue: defaultValue(getProps(schema, nextOffset)),
+      });
+    },
+    sync() {
+      this.add(this.get().nextValue);
+    },
+    add(value) {
+      const { result, keys } = this.get();
 
       const key = randId();
-      const next = keys ? keys.length : 0;
-      const value = defaultValue(getProps(schema, next));
 
       if (!result) {
         this.set({
@@ -88,20 +122,23 @@ export default {
         });
       }
     },
-    remove(offset) {
-      const { result, keys } = this.get();
-
-      result.splice(offset, 1);
-      keys.splice(offset, 1);
-
-      this.set({ result, keys });
-    },
   },
   computed: {
-    items({ schema, result, keys }) {
+    nextOffset({ result }) {
+      return result ? result.length : 0;
+    },
+    nextProps({ schema, nextOffset }) {
+      return {
+        props: getProps(schema, nextOffset),
+      };
+    },
+    values({ result }) {
+      return result || [];
+    },
+    items({ schema, values, keys }) {
       const isFixed = Array.isArray(schema.items);
 
-      return result.map((_, offset) => {
+      return values.map((_, offset) => {
         const props = getProps(schema, offset);
         const key = keys[offset] || (keys[offset] = randId());
 
