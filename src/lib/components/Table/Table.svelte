45198b3040ebf30a5d4fba92a342c974ee1836fd
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2018-12-12 01:18:07 -0600

    Cleanup

--- src/lib/components/Table/Table.svelte
+++ src/lib/components/Table/Table.svelte
@@ -15,20 +15,21 @@
           </td>
         {/each}
       </tr>
+    {:else}
+      <tr>
+        <td colspan="99" data-empty>{fixedSchema['ui:empty'] || 'No items'}</td>
+      </tr>
     {/each}
   </tbody>
-  <tfoot>
-    <tr>
-      <th>
-        <button type="submit" on:click="append()">
-          <span>{fixedSchema['ui:new'] || 'New'}</span>
-        </button>
-        <Modal {uiSchema} resource="test" bind:visible="isOpen" on:save="add()">
-          <Field name='__ROOT__' bind:result="value" {...fieldProps} />
-        </Modal>
-      </th>
-    </tr>
-  </tfoot>
 </table>
+
+<div>
+  <button type="submit" on:click="append()">
+    <span>{fixedSchema['ui:new'] || 'New'}</span>
+  </button>
+  <Modal {uiSchema} resource="test" bind:visible="isOpen" on:save="add()">
+    <Field name='__ROOT__' bind:result="value" {...fieldProps} />
+  </Modal>
+</div>
 
 <script src="script.js"></script>
