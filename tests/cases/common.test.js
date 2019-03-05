947f679bc97b1d508245a18c391f473f169523f4
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-03-05 16:13:33 -0600

    Cleanup

--- tests/cases/common.test.js
+++ tests/cases/common.test.js
@@ -1,15 +1,20 @@
 import 'testcafe';
 
 import defaultPage from '../pages/default';
-import Formator from '../components/formator';
+import { getForm } from '../components/formator';
 
 /* global fixture, test */
-
-const $ = new Formator('test1');
 
 fixture('Test #1 - Empty config')
   .page(defaultPage.url('#test1'));
 
 test('should display a "Missing props" error', async t => {
-  await t.expect($.failure.visible).ok();
+  await t.expect(getForm('test1').failure.visible).ok();
+});
+
+fixture('Test #2 - Type definitions')
+  .page(defaultPage.url('#test2'));
+
+test('should handle integer types', async t => {
+  await t.expect(getForm('test2').field('/intValue').visible).ok();
 });
--- tests/components/formator.js
+++ tests/components/formator.js
@@ -3,6 +3,24 @@ import { Selector } from 'testcafe';
 export default class Formator {
   constructor(rootId) {
     this.jsonForm = Selector(`#${rootId} + .formator`);
-    this.failure = this.jsonForm.find('[data-failure]');
+    this.failure = this._get('[data-failure]');
   }
+
+  _get(selector) {
+    this._cache = this._cache || {};
+
+    if (!this._cache[selector]) {
+      this._cache[selector] = this.jsonForm.find(selector);
+    }
+
+    return this._cache[selector];
+  }
+
+  field(propName) {
+    return this._get(`[data-field="${propName}"]`);
+  }
+}
+
+export function getForm(formId) {
+  return new Formator(formId);
 }
