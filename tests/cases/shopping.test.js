3e4f6fba7f815f3e9ab7a6c4285c573125b716ad
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-03-18 17:01:01 -0600

    Cleanup; test integration apart

--- tests/cases/shopping.test.js
+++ tests/cases/shopping.test.js
@@ -1,28 +1,27 @@
 import 'testcafe';
 
-import defaultPage from '../pages/default';
 import { getForm as $ } from '../components/formator';
 
 /* global fixture, test */
 
 fixture('Test #4 - Shopping Cart example')
-  .page(defaultPage.url('#test4'));
+  .page('http://localhost:8081/db/Cart');
 
 test('should start with an empty list', async t => {
-  await t.expect($('test4').empty.visible).ok();
+  await t.expect($('.formator').empty.visible).ok();
 });
 
 test('should create Cart(s) with Product(s)-as-items', async t => {
   // FIXME: simplify DSL for testing...
-  await t.click($('test4').is('new'));
-  await t.click($('test4').is('append'));
-  await t.typeText($('test4').is('finder'), 'abc', { replace: true });
-  await t.wait(150).click($('test4').get('[data-autocomplete] li').nth(0));
-  await t.expect($('test4').get('[name="items[0][Product][name]"]').value).contains('Some product');
-  await t.typeText($('test4').get('[name="items[0][qty]"]'), '1', { replace: true });
-  await t.click($('test4').is('save')).wait(150);
-  await t.click($('test4').is('save'));
-  await t.expect($('test4').field('/0/items').visible).ok();
-  await t.expect($('test4').field('/0/items').textContent).contains('Products bought');
-  await t.click($('test4').is('remove'));
+  await t.click($('.formator').is('new'));
+  await t.click($('.formator').is('append'));
+  await t.typeText($('.formator').is('finder'), 'abc', { replace: true });
+  await t.wait(150).click($('.formator').get('[data-autocomplete] li').nth(0));
+  await t.expect($('.formator').get('[name="items[0][Product][name]"]').value).contains('Some product');
+  await t.typeText($('.formator').get('[name="items[0][qty]"]'), '1', { replace: true });
+  await t.click($('.formator').is('save')).wait(150);
+  await t.click($('.formator').is('save'));
+  await t.expect($('.formator').field('/0/items').visible).ok();
+  await t.expect($('.formator').field('/0/items').textContent).contains('Products bought');
+  await t.click($('.formator').is('remove'));
 });
