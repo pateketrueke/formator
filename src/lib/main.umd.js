c61a13eed635e58127641720f48802e34ab85056
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-10-20 11:00:37 -0500

    Lock deps

--- src/lib/main.umd.js
+++ src/lib/main.umd.js
@@ -5,8 +5,9 @@ $bundle: Formator
 ---
 */
 
-export { loader, Form, Table } from './main.js';
-import { loader as loader$ } from './main.js';
+import { loader as loader$ } from './main';
+
+export { loader, Form, Table } from './main';
 
 if (typeof window !== 'undefined') {
   loader$('script[rel=resource],div[data-resource]');
