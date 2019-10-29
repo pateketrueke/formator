1d71cd70f6fd7abf03f584c8e14dc3478afacad7
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2019-10-29 09:51:50 -0600

    Setup some fixtures

--- e2e/server.js
+++ e2e/server.js
@@ -9,7 +9,14 @@ const repo = new Formator(new Sequelizer({
   storage: ':memory:',
 }));
 
+const fixtures = [
+  { title: 'Hello World!' },
+];
+
 repo.database.add({
+  $attributes: {
+    findAll: ['title'],
+  },
   $schema: {
     id: 'Example',
     properties: {
@@ -23,6 +30,7 @@ repo.database.add({
 Promise.resolve()
   .then(() => repo.database.connect())
   .then(() => repo.database.sync())
+  .then(() => repo.database.models.Example.bulkCreate(fixtures))
   .then(() => {
     app.use('/db', repo.hook());
     app.listen(8080);
