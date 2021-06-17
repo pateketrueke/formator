b49da49ef3019159ce7137710d19df0195805353
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-06-17 00:13:04 -0500

    fix: embed search-icon through css only

--- src/lib/components/Finder/Finder.svelte
+++ src/lib/components/Finder/Finder.svelte
@@ -57,11 +57,7 @@
   }
 </script>
 
-<Search {data} {fallback} {placeholder} on:input={search} on:change={sync} bind:value nofilter autoclose>
-  <svg slot="before" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
-    <path fill-rule="evenodd" d="M14.53 15.59a8.25 8.25 0 111.06-1.06l5.69 5.69a.75.75 0 11-1.06 1.06l-5.69-5.69zM2.5 9.25a6.75 6.75 0 1111.74 4.547.746.746 0 00-.443.442A6.75 6.75 0 012.5 9.25z"></path>
-  </svg>
-</Search>
+<Search {data} {fallback} {placeholder} on:input={search} on:change={sync} bind:value nofilter autoclose />
 
 {#if value.length > 0}
   <div data-selected>
