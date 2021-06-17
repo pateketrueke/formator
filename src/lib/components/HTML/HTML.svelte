4120683c7ac29cf3b07e32152f9f4d58e5b036ff
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-06-17 05:49:45 -0500

    fix: revert patching, nodes are being recreated due refs

--- src/lib/components/HTML/HTML.svelte
+++ src/lib/components/HTML/HTML.svelte
@@ -1,13 +1,9 @@
 <script>
   import { onMount, onDestroy } from 'svelte';
-  import {
-    $ as render, patch, mount, unmount,
-  } from '../Value/widgets';
+  import { $ as render, mount, unmount } from '../Value/widgets';
 
   export let vnode;
 
-  let dirty;
-  let prev;
   let ref;
 
   onMount(() => {
@@ -17,10 +13,6 @@
   onDestroy(() => {
     unmount(ref);
   });
-
-  $: if (prev && prev !== vnode) {
-    patch(ref, prev, prev = vnode, render);
-  }
 </script>
 
 <div data-ref bind:this={ref} />
