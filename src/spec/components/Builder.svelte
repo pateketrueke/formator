bfeec52ddddeba1fe79919ac9bd1e339b0d697a2
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-06-28 11:51:25 -0500

    test: improved builder UI

--- src/spec/components/Builder.svelte
+++ src/spec/components/Builder.svelte
@@ -1,8 +1,12 @@
 <script>
+  import { Fence } from 'smoo';
   import { Schema, Input } from '../../lib/main.js';
 
   let status = 'pristine';
   let valid = false;
+  let broken;
+  let modal;
+  let visible;
 
   function save(e) {
     alert(JSON.stringify(e.detail, null, 2));
@@ -11,39 +15,65 @@
     valid = e.detail.valid;
     status = e.detail.valid ? 'valid' : 'invalid';
   }
+  function toggle() {
+    visible = !visible;
+    if (!visible) modal = false;
+  }
+  function overlay(e) {
+    modal = e.target.checked;
+  }
+  function invalid() {
+    broken = !broken;
+  }
 </script>
 
 <details>
   <summary id="test-builder">Form builder</summary>
-  <Schema debug on:submit={save} on:change={check}>
-    <Input
-      name="email"
-      label="E-mail"
-      format="email"
-      required
-      autocomplete="current-email"
-    />
-    <Input
-      name="password"
-      label="New password"
-      password
-      required
-      autocomplete="current-password"
-    />
-    <Input
-      name="new_password"
-      label="Confirm password"
-      password
-      required
-      autocomplete="confirm-password"
-    />
-    <slot slot="actions">
-      <input type="submit" value="Save" disabled={!valid} />
-      <span class="fill">{status}</span>
-      <span class="flex gap">
-        <label for="validity" class="min">checkValidity</label>
-        <input id="validity" type="checkbox" bind:checked={valid} />
-      </span>
-    </slot>
-  </Schema>
+  <button class="nosel" on:click={toggle}>Toggle form</button>
+  <Fence autofocus bind:visible bind:modal on:cancel={toggle}>
+    <div class="formator" slot="main">
+      <Schema debug on:submit={save} on:change={check}>
+        {#if broken}<Input />{/if}
+        <Input
+          name="email"
+          label="E-mail"
+          format="email"
+          required
+          autocomplete="current-email"
+        />
+        <Input
+          name="password"
+          label="New password"
+          password
+          required
+          autocomplete="current-password"
+        />
+        <Input
+          name="new_password"
+          label="Confirm password"
+          password
+          required
+          autocomplete="confirm-password"
+        />
+        <slot slot="actions">
+          <input type="submit" value="Save" disabled={!valid} />
+          <span class="fill">{status}</span>
+          <span class="flex gap x2">
+            <label class="flex gap min">
+              <input type="checkbox" on:change={overlay} />
+              modal
+            </label>
+            <label class="flex gap min">
+              <input type="checkbox" on:change={invalid} />
+              broken
+            </label>
+            <label class="flex gap min">
+              <input type="checkbox" bind:checked={valid} />
+              validate
+            </label>
+          </span>
+        </slot>
+      </Schema>
+    </div>
+  </Fence>
 </details>
