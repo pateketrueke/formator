7a4733121995a90d6b8561da7bebc9f6556d99ba
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-06-29 14:35:42 -0500

    fix: pass all supported props for Input

--- src/lib/components/Form/Input.svelte
+++ src/lib/components/Form/Input.svelte
@@ -3,11 +3,26 @@
 
   export let type = 'string';
   export let name = null;
+  export let hint = null;
+  export let cols = null;
+  export let ros = null;
+  export let size = null;
+  export let title = null;
   export let label = null;
   export let format = null;
-  export let required = null;
+  export let toggle = null;
+  export let hidden = null;
+  export let columns = null;
+  export let textarea = null;
   export let password = null;
+  export let required = null;
+  export let multiple = null;
+  export let template = null;
+  export let maxlength = null;
   export let autocomplete = null;
+
+  let className;
+  export { className as class };
 
   let defaultValue;
   export { defaultValue as value };
@@ -23,8 +38,24 @@
 
     $value[name] = defaultValue;
     $uiSchema[name] = {};
+
+    if (className) $uiSchema[name]['ui:class'] = className;
+    if (hint) $uiSchema[name]['ui:hint'] = hint;
+    if (cols) $uiSchema[name]['ui:cols'] = cols;
+    if (ros) $uiSchema[name]['ui:ros'] = ros;
+    if (size) $uiSchema[name]['ui:size'] = size;
+    if (title) $uiSchema[name]['ui:title'] = title;
     if (label) $uiSchema[name]['ui:label'] = label;
+    if (format) $uiSchema[name]['ui:format'] = format;
+    if (toggle) $uiSchema[name]['ui:toggle'] = toggle;
+    if (hidden) $uiSchema[name]['ui:hidden'] = hidden;
+    if (columns) $uiSchema[name]['ui:columns'] = columns;
+    if (textarea) $uiSchema[name]['ui:textarea'] = textarea;
     if (password) $uiSchema[name]['ui:password'] = password;
+    if (required) $uiSchema[name]['ui:required'] = required;
+    if (multiple) $uiSchema[name]['ui:multiple'] = multiple;
+    if (template) $uiSchema[name]['ui:template'] = template;
+    if (maxlength) $uiSchema[name]['ui:maxlength'] = maxlength;
     if (autocomplete) $uiSchema[name]['ui:autocomplete'] = autocomplete;
   }
 </script>
