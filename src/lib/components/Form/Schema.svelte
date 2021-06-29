adb47200e9814d7574fb9c47a325a9bdc81e55ff
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-06-29 13:22:48 -0500

    fix: enable simple validation callback as Schema prop

--- src/lib/components/Form/Schema.svelte
+++ src/lib/components/Form/Schema.svelte
@@ -4,6 +4,7 @@
   import Form from './Form.svelte';
 
   export let debug = false;
+  export let validate = null;
 
   const dispatch = createEventDispatcher();
   const uiSchema = writable({});
@@ -21,19 +22,26 @@
     uiSchema,
   });
 
+  function validation() {
+    if (typeof validate !== 'function') return true;
+    return validate($value, $schema, $uiSchema);
+  }
+
   function update(e) {
+    if (!validation()) return;
     value.set(e.detail);
     dispatch('submit', e.detail);
   }
 
-  function validate(e) {
+  function check(e) {
+    if (!validation()) return;
     dispatch('change', e.detail);
   }
 </script>
 
 <slot />
 
-<Form result={$value} schema={$schema} uiSchema={$uiSchema} on:submit={update} on:change={validate}>
+<Form result={$value} schema={$schema} uiSchema={$uiSchema} on:submit={update} on:change={check}>
   <div slot="after" data-actions>
     <slot name="actions">
       <button type="submit">Save</button>
